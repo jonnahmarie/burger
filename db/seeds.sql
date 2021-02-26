2b96a97a29dd767585300f6439a4e11740942be0
jonnahmarie <jonnah.marie@gmail.com>
2021-02-26 12:59:15 -0800

    initial schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+    ("Cheeseburger", true),
+    ("Double Cheeseburger", true),
+    ("Bacon Avocado Cheeseburger", true),
+    ("Hamburger", false);
