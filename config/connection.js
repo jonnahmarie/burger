c0c1e29e84e5b2634fcc11c1c879a1631fe1c193
jonnahmarie <jonnah.marie@gmail.com>
2021-04-08 14:28:47 -0700

    adding code to connect to local host or heroku

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,17 @@
-// Set up MySQL connection.
-const mysql = require("mysql");
-const dbConfig = require("./db.config")
+const mysql = require('mysql');
+let connection;
 
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  port: process.env.PORT || 3306,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'password',
+        database: 'burgers_db'
+    });
+};
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
